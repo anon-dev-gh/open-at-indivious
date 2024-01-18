37115350632af61d048e85a70dbe67938917dacb
anon-dev <148810564+anon-dev-gh@users.noreply.github.com>
2024-01-18 21:26:14 +0100

    feat: it opens a hardcoded indivious instance when right clicking at youtube

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,29 @@
 import { runtime } from 'webextension-polyfill'
 
-runtime.onInstalled.addListener(() => {
-  console.log('[background] loaded ')
-})
+const logExtensionLoaded = () => {
+  console.log('Open at Indivous loaded.')
+}
+
+runtime.onInstalled.addListener(logExtensionLoaded)
+
+chrome.contextMenus.create({
+  id: "open-at-indivious",
+  title: "Open at indivious",
+  contexts:["link"],
+  documentUrlPatterns: ["https://www.youtube.com/*"]
+});
+
+const replaceUrl = (url: string): string => {
+  return url.replace("www.youtube.com", "vid.puffyan.us")
+}
+
+const searchUrbanDict = (info: chrome.contextMenus.OnClickData, _tab: chrome.tabs.Tab | undefined) => {
+  if (info.linkUrl) {
+    const newUrl = replaceUrl(info.linkUrl)
+    chrome.tabs.create({url: newUrl});
+  }
+};
+
+chrome.contextMenus.onClicked.addListener(searchUrbanDict)
 
 export {}
